77e6c848bca12e2884eccd4eb9fdefe739cb94b2
syimyk <sagynov512@gmail.com>
2025-05-02 19:29:51 +0600

    "first commit"

---
+++ src/main/java/com/example/transaction_outbox_pattern/TransactionOutboxPatternApplication.java
@@ -0,0 +1,13 @@
+package com.example.transaction_outbox_pattern;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransactionOutboxPatternApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TransactionOutboxPatternApplication.class, args);
+    }
+
+}
